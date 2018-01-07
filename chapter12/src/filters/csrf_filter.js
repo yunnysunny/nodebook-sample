d155a31622a3cc6f0ff8faef04ebceecf368b37b
yunnysunny <yunnysunny@gmail.com>
2018-01-07 21:23:50 +0800

    删除多余注释

--- chapter12/src/filters/csrf_filter.js
+++ chapter12/src/filters/csrf_filter.js
@@ -1,18 +1,8 @@
 var csurf = require('csurf')();
-// require('../lib/string');
-// function _needSafe(path) {
-//     if (path === '/article/add') {
-//         return true;
-//     }
-//     return false;
-// }
+
 module.exports = function(app) {
     app.use(function (req, res, next) {
-        // if (req.method === 'POST') {
-            csurf(req, res, next);
-        //     return;
-        // }
-        // next();
+        csurf(req, res, next);
     });
     app.use(function (req, res, next) {
         res.locals.csrf = req.csrfToken ? req.csrfToken() : '';
