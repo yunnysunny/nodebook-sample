9cb6672ff89f0fc7b582992521c361c6d740d87c
yunnysunny <yunnysunny@gmail.com>
2021-05-30 19:27:58 +0800

    修正在async.js3下程序提前退出的问题

--- chapter2/async_waterfull.js
+++ chapter2/async_waterfull.js
@@ -3,17 +3,17 @@ var async = require('async');
 async.waterfall([
     function(callback) {
         setTimeout(function() {
-            callback(false,2+3)
+            callback(null, 2+3)
         },100);
     },
     function(sum,callback) {
         setTimeout(function() {
-            callback(false,sum-1)
+            callback(null, sum-1)
         },100);
     },
     function(left,callback) {
         setTimeout(function() {
-            callback(false,left * 2)
+            callback(null, left * 2)
         },100);
     }
 ],function(err,result) {
